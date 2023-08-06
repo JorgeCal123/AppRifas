bd9c8befa4593018ce2a61c3c8af5da65a6e2ed9
ruizdani301 <ruizdani301@gmail.com>
2023-08-06 12:30:19 -0500

    ADD: configuracion carpeta .env

--- fastapi_Rifas/config/conexion.py
+++ fastapi_Rifas/config/conexion.py
@@ -1,8 +1,20 @@
+# import os
+# import environ
+
+# env = environ.Env(
+#     DEBUG=(bool, False)
+# )
+
+# # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# # Take environment variables from .env file
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root:1234@localhost:3306/rifas"
+DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/rifas"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
